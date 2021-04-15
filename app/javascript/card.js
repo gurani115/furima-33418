ee425cb163a90b50cd1996f0251d09d3e2a4dae6
gurani115 <kouki11x@gmail.com>
2021-04-15 22:41:53 +0900

    画像のプレビュー機能

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,7 +1,7 @@
 const pay = () => {
   Payjp.setPublicKey(process.env.PAYJP_PUBLIC_KEY);
   const form = document.getElementById("charge-form");
-  form.addEventListener("submit", (e) => {
+  form.addEventListener("submit",(e) => {
     e.preventDefault();
     const formResult = document.getElementById("charge-form");
     const formData = new FormData(formResult);
